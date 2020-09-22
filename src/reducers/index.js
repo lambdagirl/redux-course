9a98ab0bb169840e95156048040d2c364ffc711d
Peizhi Zheng <pzheng16@asu.edu>
2020-09-21 23:58:08 -0700

    add User

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux'
 import authedUser from './authedUser'
+import User from "./user";
 
 export default combineReducers({
-    authedUser
-})
+  authedUser,
+  User,
+});
