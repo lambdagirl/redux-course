c6e026837d574eef67142a2c4e39a6d3f3aafa8d
Peizhi Zheng <pzheng16@asu.edu>
2020-09-24 06:26:44 -0700

    add loading

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import authedUser from './authedUser'
-import User from "./user";
+import users from "./users";
+import { loadingBarReducer } from 'react-redux-loading'
 
 export default combineReducers({
   authedUser,
-  User,
+  users,
+  loadingBarReducer
 });
